6df5335b67e1510f704b454067185ea00d17f1e3
wangxing5320 <1291878077@qq.com>
2018-08-20 18:43:38 +0800

    二级分类的表单验证图片实时预览添加功能都已完成，二级分类的功能已经全部完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -51,20 +51,97 @@ $(function(){
 
         //给分类信息通过事件委托注册点击事件
         $("#cate").on("click","a",function(){
-        $("#dropdownMenu1").text($(this).text());  
+        $("#dropdownMenu1").text($(this).text());
+        //将点击的 一级分类 的id赋值给input的value值将来好提交到后台
+        $("[name='categoryId']").val($(this).data("id"));
+        
+        $("#form").data("bootstrapValidator").updateStatus("categoryId","VALID"); 
         })
       }
     })
   })
-//点击上传图片的按钮时，图片实时显示
-$("#pic").on('click',function(){
+  
+//初始化图片上传插件，图片实时显示
+
   $("#pic1").fileupload({
     dataType:"json",
     done:function(e,data){
-     $("#im").attr("src",data.result.picAddr) ;
-      console.log(data);
+     $("#im").attr("src",data.result.picAddr);
+     $("[name='brandLogo']").val(data.result.picAddr);
+                                      
+     $("#form").data("bootstrapValidator").updateStatus("brandLogo","VALID");
+       
+    }
+  })
+
+
+//提交表但时验证表单
+//初始化表单插件
+ 
+$("#form").bootstrapValidator({
+           
+  //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
+  excluded:[],
+   // 配置校验图标
+   feedbackIcons: {
+    valid: 'glyphicon glyphicon-ok',    // 校验成功
+    invalid: 'glyphicon glyphicon-remove',  // 校验失败
+    validating: 'glyphicon glyphicon-refresh' // 校验中
+  },
+ 
+  fields:{
+    categoryId:{
+      validators:{
+        notEmpty:{
+          message:"请选择一级分类",
+        }
+      }
+    },
+   
+    brandName:{
+      validators:{
+        notEmpty:{
+          message:"请输入二级分类"
+        }
+      }
+    },
+    brandLogo:{
+      validators:{
+        notEmpty:{
+          message:"请上传图片"
+        }
+      }
+    }
+
+  }
+})
+//验证成功后
+$("#form").on("success.form.bv",function(e){
+  e.preventDefault();
+  $.ajax({
+    type:"post",
+    url:"/category/addSecondCategory",
+    data:$("#form").serialize(),
+    dataType:"json",
+    success:function(info){
+      if(info.success){
+        //关闭模态框
+        $("#secondtModal").modal("hide");
+        currentPage=1;//页面跳转到首页(好观察添加的信息);
+        //重新渲染页面
+        rand();
+        //重置表单
+        $("#form").data("bootstrapValidator").resetForm(true);
+        //重置按钮文本
+        $("#dropdownMenu1").text("请选择一级分类");
+        //重置图片
+        $("#im").attr("src","./images/none.png");
+
+      }
+      console.log(info)
     }
   })
 })
+
 
 })
